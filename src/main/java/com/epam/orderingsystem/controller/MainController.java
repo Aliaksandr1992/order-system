d8963ca4eeb9d63f2c28fbd78af5f02922e63501
Aliaksandr1992 <alexander.medvedev.92@gmail.com>
2021-02-17 12:28:11 +0100

    Add wish controller and html file for showing all wishes.

---
+++ src/main/java/com/epam/orderingsystem/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.epam.orderingsystem.controller;
+
+import com.epam.orderingsystem.model.Child;
+import com.epam.orderingsystem.model.GiftOrder;
+import com.epam.orderingsystem.model.Wish;
+import com.epam.orderingsystem.processor.WishProcessor;
+import com.epam.orderingsystem.service.ChildService;
+import com.epam.orderingsystem.service.GiftOrderService;
+import com.epam.orderingsystem.service.WishBuilderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private WishBuilderService wishBuilderService;
+    @Autowired
+    private ChildService childService;
+    @Autowired
+    private GiftOrderService giftOrderService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showAllWishes(Model model)
+    {
+        List<Wish> wishes = wishBuilderService.build(childService.findAllChildren(), giftOrderService.findAllOders());
+
+        model.addAttribute("wishes", wishes);
+        return "index";
+    }
+}
