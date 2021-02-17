e5ea80f9e27016e372aec4e816d6b3cfb845a6fd
Aliaksandr1992 <alexander.medvedev.92@gmail.com>
2021-02-17 12:29:45 +0100

    Add comments to methods

--- src/main/java/com/epam/orderingsystem/service/WishBuilderService.java
+++ src/main/java/com/epam/orderingsystem/service/WishBuilderService.java
@@ -7,5 +7,11 @@ import com.epam.orderingsystem.model.Wish;
 import java.util.List;
 
 public interface WishBuilderService {
+    /**
+     * Build wish from children and order
+     * @param children is list of all children
+     * @param orders is list of all orders
+     * @return list of wishes
+     */
     List<Wish> build(List<Child> children, List<GiftOrder> orders);
 }
